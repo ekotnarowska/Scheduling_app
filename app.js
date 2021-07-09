6d2ed4ca9a52de4ffcda04822653b5e8e455359e
ekotnarowska <ekotnarowska@gmail.com>
2021-07-09 10:10:18 +0200

    handlebars views and layout

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const exphbs = require('express-handlebars');
 const crypto = require('crypto');
-const router = express.Router();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    helpers: {
+        counter: (index) => index + 1,
+    
+    }
+})
 
 const { users, schedules } = require('./data')
 
 module.exports = app;
 
+//Handlebars Middleware
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
-// app.post('/users', (req, res) => {
-//     res.json({message: req.body})
-// })
+//HomePage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Schedule website',
+    subtitle: 'Welcome to our website'
+}));
 
-app.get('/', (req, res,) => {
-    res.send('<h1>Welcome to our schedule website<h1>')
-})
+
+//Users Route
+app.get('/users', (req, res) => res.render('users', {
+    title: 'Users',
+    users
+}));
+
+//New user Route
+
+app.get('/users/new', (req, res) => res.render('user_form'));
+
+
+//Single User Route
+// app.get('/users/:id', (req, res) => res.render('user', {
+//     title: 'User'
+// }));
+
+//schedules Route
+app.get('/schedules', (req, res) => res.render('schedules', {
+    title: 'Schedules',
+    schedules
+}));
+
+//New Schedule Route
+app.get('/schedules/new', (req, res) => res.render('schedule_form'));
+
+// app.get('/', (req, res,) => {
+//     res.send('<h1>Welcome to our schedule website<h1>')
+// })
 
 //Get all users
 app.get('/users', (req, res) => {
@@ -28,7 +70,7 @@ app.get('/users', (req, res) => {
 
 //Get all schedules
 app.get('/schedules', (req, res) => {
-    res.send(schedules)
+    res.send(schedules, users)
 })
 
 //Get single user
@@ -37,9 +79,18 @@ app.get('/users/:userId', (req, res) => {
     const user = users[userId]
 
     if(user === undefined) {
-        res.status(404).send(`Incorrect user id: ${userId}` )
+        // res.status(404).send(`Incorrect user id: ${userId}` )
+        res.render('error', {
+            userError: true,
+            event: 'user',
+            id: userId
+        })
     }
-    res.json(user)
+    // res.json(user)
+    res.render('user', {
+        title: 'User',
+        user
+    })
 })
 
 //Get schedules for user
@@ -50,7 +101,12 @@ app.get('/users/:userId/schedules', (req, res) => {
     if(found) {
         res.json(schedules.filter(schedule => schedule.user_id === parseInt(userId)));
     } else {
-        res.status(404).send(`No schedule for user with the the id of: ${userId}`)
+        // res.status(404).send(`No schedule for user with the the id of: ${userId}`)
+        res.render('error', {
+            scheduleError: true,
+            event: 'schedule',
+            id: userId})
+
     }
 })
 
@@ -77,7 +133,12 @@ app.post('/users', (req, res) => {
         password: crypto_password
     }
     users.push(newUser)
-    res.json(users)
+    // res.json(users)
+    res.redirect('/users')
+})
+
+app.use((req, res, next) => {
+    res.status(404).render('error')
 })
 
 
---
+++ config/handlebars-helpers.js
@@ -0,0 +1,7 @@
+// const exphbs = require('express-handlebars');
+
+// Handlebars.registerHelper("counter", function (index){
+//     return index + 1;
+// });
+
+// module.exports = counter;
